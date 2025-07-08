9840fb2fbe146979db18a29ec9b3dcaed59e51dd
hynek.braun <hynek.braun@livesport.eu>
2025-07-08 14:47:21 +0200

    fix ios detail visuals (a little bit)

--- iosApp/iosApp/Features/CharacterDetail.swift
+++ iosApp/iosApp/Features/CharacterDetail.swift
@@ -3,29 +3,28 @@ import SwiftUI
 
 struct CharacterDetail: View {
 	@Environment(\.dismiss) private var dismiss
-
 	private let characterId: String
-
+	
 	@StateObject private var viewModel: SharedViewModelStoreOwner<CharacterDetailViewModel>
-
+	
 	init(characterId: String) {
 		self.characterId = characterId
+		
 		self._viewModel = StateObject(wrappedValue:
-					SharedViewModelStoreOwner {
-						CharacterDetailViewModel(
-							charactersRepository: inject(),
-							favoritesRepository: inject(),
-							characterId: characterId,
-							componentFactory: inject(),
-							drawableResources: inject()
-						)
-					}
+			SharedViewModelStoreOwner {
+				CharacterDetailViewModel(
+					charactersRepository: inject(),
+					favoritesRepository: inject(),
+					characterId: characterId,
+					componentFactory: inject(),
+					drawableResources: inject()
 				)
-		}
-
+			}
+		)
+	}
+	
 	var body: some View {
 		VStack {
-		
 			Observing(viewModel.instance.actionBarState) { actionBarState in
 				ActionBarComponent(
 					model: actionBarState,
@@ -38,34 +37,34 @@ struct CharacterDetail: View {
 				switch onEnum(of: state) {
 				case let .data(state):
 					content(model: state)
-
 				case .error:
 					ErrorMessage(error: "Something went wrong")
-
 				case .loading:
 					Loading()
 				}
 			}
 		}
+		.navigationBarHidden(true) // Move this to the main body
 	}
-
+	
 	@ViewBuilder
 	private func content(model: CharacterDetailViewState.Data) -> some View {
 		ScrollView {
-			LazyVStack(spacing: 16) {
+			LazyVStack(alignment: .leading, spacing: 16) { // Add alignment: .leading
 				DetailHeaderComponent(model: model.header)
-
+				
 				ForEach(model.info, id: \.title) { infoItem in
 					InformationComponent(model: infoItem)
 						.padding(.horizontal, 16)
+						.frame(maxWidth: .infinity, alignment: .leading)
 				}
 			}
+			.frame(maxWidth: .infinity, alignment: .leading)
 			.padding(.top, 16)
 			.padding(.bottom, 24)
+			.background(.backgroundSecondary)
+			.cornerRadius(16)
 		}
-		.navigationBarHidden(true)
 		.padding(.horizontal, 8)
-		.tint(.backgroundSecondary)
-		.cornerRadius(16)
 	}
 }
